608d6aeffd29246ee0f9c3c6d39aa5a064582d7d
Johan Lundgren <johan.lundgren97@me.com>
2016-01-11 19:47:43 +0100

    Initial Commit

---
+++ Euler9/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Euler9
+//
+//  Created by JohanLundgren on 2016-01-11.
+//  Copyright © 2016 SuperMilkApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
